922ade86ec1f09d2caa4493ec793d1c8bf752428
lm0x86 <liviu.maftuleac@opentech.dev>
2024-07-01 08:45:13 +0300

    add run script

---
+++ backend/run.js
@@ -0,0 +1,10 @@
+const { exec } = require('child_process');
+
+exec('npm start', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+  console.error(stderr);
+});
